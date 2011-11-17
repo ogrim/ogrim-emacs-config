647c6ec6cc9b604e256bc8aa9f3a7aa8d148d486
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-11-17 11:49:16 +0100

    Updating sexp-flashing with zenburn colors

--- live-config/highlight-flash-conf.el
+++ live-config/highlight-flash-conf.el
@@ -1,7 +1,6 @@
 ;; highlight expression on eval
 (require 'highlight)
 (require 'eval-sexp-fu)
-(setq eval-sexp-fu-flash-duration 0.5)
-
-
-
+(setq eval-sexp-fu-flash-duration 0.2)
+(setq eval-sexp-fu-flash-face
+      '((((class color)) (:background "#656555" :foreground "#dcdccc" :bold t))))
